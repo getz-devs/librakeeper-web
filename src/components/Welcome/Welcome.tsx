9d21bdbf2c1264e878af725f17a6dfcfa0d66c62
LordLizard Notebook <WarriorXKZ@gmail.com>
2024-06-22 20:39:51 +0300

    feat: change formatter

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -6,13 +6,18 @@ export function Welcome() {
         <>
             <Title className={classes.title} ta="center" mt={100}>
                 Welcome to{' '}
-                <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
+                <Text
+                    inherit
+                    variant="gradient"
+                    component="span"
+                    gradient={{ from: 'pink', to: 'yellow' }}
+                >
                     Mantine
                 </Text>
             </Title>
             <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-                This starter Next.js project includes a minimal setup for server side rendering,
-                if you want to learn more on Mantine + Next.js integration follow{' '}
+                This starter Next.js project includes a minimal setup for server side rendering, if
+                you want to learn more on Mantine + Next.js integration follow{' '}
                 <Anchor href="https://mantine.dev/guides/next/" size="lg">
                     this guide
                 </Anchor>
