4a9f53355e42f765589e5e05fc15e64364876bc8
LordLizard Notebook <WarriorXKZ@gmail.com>
2024-06-22 20:11:57 +0300

    fix: change some rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,19 @@
 module.exports = {
-  extends: ['mantine', 'plugin:@next/next/recommended', 'plugin:jest/recommended'],
-  plugins: ['testing-library', 'jest'],
-  overrides: [
-    {
-      files: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-      extends: ['plugin:testing-library/react'],
+    extends: ['mantine', 'plugin:@next/next/recommended', 'plugin:jest/recommended'],
+    plugins: ['testing-library', 'jest'],
+    overrides: [
+        {
+            files: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+            extends: ['plugin:testing-library/react'],
+        },
+    ],
+    parserOptions: {
+        project: './tsconfig.json',
     },
-  ],
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'import/extensions': 'off',
-  },
+    rules: {
+        'react/jsx-indent-props': 'off',
+        'linebreak-style': 0,
+        'react/react-in-jsx-scope': 'off',
+        'import/extensions': 'off',
+    },
 };
