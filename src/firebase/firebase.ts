be22305ad41985ad4941e79605448a100b3e303f
LordLizard Notebook <WarriorXKZ@gmail.com>
2024-06-27 17:23:31 +0300

    fix: fix firebase

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -14,11 +14,11 @@ import {
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
-    authDomain: 'librakeeper-web.firebaseapp.com',
-    projectId: 'librakeeper-web',
-    storageBucket: 'librakeeper-web.appspot.com',
-    messagingSenderId: '317795782010',
-    appId: '1:317795782010:web:b1b6239ea87c20d8599ac3',
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
