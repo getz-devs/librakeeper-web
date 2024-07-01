dd75075f84d8372dc1107bc227596c9038ebf343
LordLizard Notebook <WarriorXKZ@gmail.com>
2024-07-02 00:48:11 +0300

    feat: try to make input

--- src/components/Html5qrcode/QRcodeCamera.tsx
+++ src/components/Html5qrcode/QRcodeCamera.tsx
@@ -1,25 +1,35 @@
 'use client';
 
+import { useState } from 'react';
+import { Input } from '@mantine/core';
 import Html5QrcodePlugin from './Html5QrcodeScannerPlugin';
 import classes from './QRcodeCamera.module.css';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const onNewScanResult = (decodedText: any, decodedResult: any) => {
-    // handle decoded results here
-    console.log(`Scan result: ${decodedText}`);
-    alert(`Scan result: ${decodedText}`);
-};
-
 export function QRcodeCamera() {
+    const [scanResult, setScanResult] = useState('');
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const onNewScanResult = (decodedText : any, decodedResult : any) => {
+        // handle decoded results here
+        console.log(`Scan result: ${decodedText}`);
+        alert(`Scan result: ${decodedText}`);
+        setScanResult(decodedText); // Записываем результат в состояние
+    };
+
     return (
-        <div className={classes.camera}>
-            <Html5QrcodePlugin
-                fps={10}
-                qrbox={260}
-                disableFlip={false}
-                aspectRatio={1.0}
-                qrCodeSuccessCallback={onNewScanResult}
-            />
+        <div>
+            <div className={classes.camera}>
+                <Html5QrcodePlugin
+                    fps={10}
+                    qrbox={260}
+                    disableFlip={false}
+                    aspectRatio={1.0}
+                    qrCodeSuccessCallback={onNewScanResult}
+                />
+            </div>
+            <div className={classes.camera}>
+                <Input placeholder="Input component" value={scanResult} />
+            </div>
         </div>
     );
 }
