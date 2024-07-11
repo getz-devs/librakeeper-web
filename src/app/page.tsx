a7e508198896c06b76eed005e18e6e839f96ddb0
L0rdLizard <WarriorXKZ@gmail.com>
2024-07-11 22:25:18 +0300

    feat: vertical list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Carousel from '@/src/components/Carousel/Carousel';
-import AddButton from '../components/AddButton/AddButton';
+import AddButton from '@/src/components/AddButton/AddButton';
+import VerticalList from '@/src/components/VerticalList/VerticalList';
 
 export default function HomePage() {
     return (
         <>
             <Carousel />
+            <VerticalList />
             <AddButton />
             {/* <ColorSchemeToggle /> */}
         </>
---
+++ src/components/VerticalList/VerticalListForm.tsx
@@ -0,0 +1,55 @@
+import { Text, Button, Stack, Card, Group, Image } from '@mantine/core';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useRef } from 'react';
+import classes from './VerticalListForm.module.css';
+
+interface Item {
+    id: string;
+    image: string;
+    title: string;
+    text: string;
+}
+
+interface ListProps {
+    items: Item[];
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function BookCard({ id, image, title, text }: Item) {
+    return (
+        <Card shadow="sm" padding={0} radius="md" withBorder style={{ width: 600 }}>
+            <Group wrap="nowrap" gap={0}>
+                <Image src={image} />
+                <Stack px="md" h="100%" style={{ width: '100%' }}>
+                    <Group justify="space-between" mb="xs">
+                        <Text fw={500}>{title}</Text>
+                    </Group>
+
+                    <Text size="sm" c="dimmed">
+                        {text}
+                    </Text>
+
+                    <Stack align="end">
+                        <Button color="blue" fullWidth mt="md" radius="md" style={{ width: '50%' }}>
+                            Choose this
+                        </Button>
+                    </Stack>
+                </Stack>
+            </Group>
+        </Card>
+    );
+}
+
+const ListOfCards: React.FC<ListProps> = ({ items }) => {
+    const viewport = useRef<HTMLDivElement>(null);
+    return (
+        <div ref={viewport} className={classes.list}>
+            {items.map((item) => (
+                <BookCard key={item.id} {...item} />
+            ))}
+        </div>
+    );
+};
+
+export default ListOfCards;
