862382b9105e85836a15525944c8a72f92d37a07
L0rdLizard <WarriorXKZ@gmail.com>
2024-07-17 18:43:09 +0300

    fix

---
+++ src/components/CarouselCards/CarouselCards.tsx
@@ -0,0 +1,106 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Loader, Title } from '@mantine/core';
+import InfiniteScrollArea from './InfiniteList';
+import { Bookshelf } from '@/src/types/api';
+
+interface Item {
+    id: string;
+    image: string;
+    title: string;
+    text: string;
+}
+
+interface CarouselProps {
+    bookshelf: Bookshelf;
+}
+
+function CarouselCards(params : CarouselProps) {
+    const { bookshelf } = params;
+    const [items, setItems] = useState<Item[]>(
+        Array.from({ length: 20 }, (_, i) => ({
+            title: `SuperJet ${i + 100}`,
+            text: `This is the text for item ${i + 1}`,
+            // image: 'https://animego.online/uploads/posts/2020-07/1593634011-volchica-i-pryanosti-poster.jpg',
+            image: 'https://sun9-16.userapi.com/impf/c637718/v637718963/51fd9/nACCl1pDqvM.jpg?size=222x314&quality=96&sign=e71b016beb5daa4c8f7a968c83636cf2&type=album',
+            id: `${i + 1}`,
+        }))
+    );
+
+    const fetchMoreData = () =>
+        new Promise<void>((resolve) => {
+            setTimeout(() => {
+                setItems((prevItems) => [
+                    ...prevItems,
+                    ...Array.from({ length: 20 }, (_, i) => ({
+                        title: `SuperJet ${prevItems.length + i + 100}`,
+                        text: `This is the text for item ${prevItems.length + i + 1}`,
+                        // image: 'https://animego.online/uploads/posts/2020-07/1593634011-volchica-i-pryanosti-poster.jpg',
+                        image: 'https://sun9-16.userapi.com/impf/c637718/v637718963/51fd9/nACCl1pDqvM.jpg?size=222x314&quality=96&sign=e71b016beb5daa4c8f7a968c83636cf2&type=album',
+                        id: `${prevItems.length + i + 1}`,
+                    })),
+                ]);
+                resolve();
+            }, 1500);
+        });
+
+    return (
+        // items
+        <div>
+            <Title order={2} ms={10} mb="xs" fw="bold">
+                Favorites
+            </Title>
+            <InfiniteScrollArea
+                items={items}
+                fetchMoreData={fetchMoreData}
+                loader={<Loader size="md" />}
+            />
+        </div>
+    );
+}
+
+export default CarouselCards;
+
+// const ExampleList: React.FC<CarouselProps> = ({ bookshelfs }) => {
+//     const [items, setItems] = useState<Item[]>(
+//         Array.from({ length: 20 }, (_, i) => ({
+//             title: `SuperJet ${i + 100}`,
+//             text: `This is the text for item ${i + 1}`,
+//             // image: 'https://animego.online/uploads/posts/2020-07/1593634011-volchica-i-pryanosti-poster.jpg',
+//             image: 'https://sun9-16.userapi.com/impf/c637718/v637718963/51fd9/nACCl1pDqvM.jpg?size=222x314&quality=96&sign=e71b016beb5daa4c8f7a968c83636cf2&type=album',
+//             id: `${i + 1}`,
+//         }))
+//     );
+
+//     const fetchMoreData = () =>
+//         new Promise<void>((resolve) => {
+//             setTimeout(() => {
+//                 setItems((prevItems) => [
+//                     ...prevItems,
+//                     ...Array.from({ length: 20 }, (_, i) => ({
+//                         title: `SuperJet ${prevItems.length + i + 100}`,
+//                         text: `This is the text for item ${prevItems.length + i + 1}`,
+//                         // image: 'https://animego.online/uploads/posts/2020-07/1593634011-volchica-i-pryanosti-poster.jpg',
+//                         image: 'https://sun9-16.userapi.com/impf/c637718/v637718963/51fd9/nACCl1pDqvM.jpg?size=222x314&quality=96&sign=e71b016beb5daa4c8f7a968c83636cf2&type=album',
+//                         id: `${prevItems.length + i + 1}`,
+//                     })),
+//                 ]);
+//                 resolve();
+//             }, 1500);
+//         });
+
+//     return (
+//         // items
+//         <div>
+//             <Title order={2} ms={10} mb="xs" fw="bold">
+//                 Favorites
+//             </Title>
+//             <InfiniteScrollArea
+//                 items={items}
+//                 fetchMoreData={fetchMoreData}
+//                 loader={<Loader size="md" />}
+//             />
+//         </div>
+//     );
+// };
--- src/components/Collections/Collections.tsx
+++ src/components/Collections/Collections.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 'use client';
 
 import { useState, useEffect } from 'react';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
 import { Card, Stack, Image, Group, Text, Badge, Button } from '@mantine/core';
 import Link from 'next/link';
 import { Bookshelf } from '@/src/types/api';
 import { useAuthContext } from '@/src/firebase/context';
+import CarouselCards from '../CarouselCards/CarouselCards';
 
 const API_HOST = process.env.NEXT_PUBLIC_API_HOST || 'http://localhost:8080/api';
 
@@ -67,22 +70,28 @@ export default function AllBookshelvesPage() {
         //     </Card>
         // </div>
 
+        // <div>
+        //     {bookshelves.map((shelf) => (
+        //         <Card shadow="sm" padding="lg" radius="md" withBorder style={{ width: '60%' }}>
+        //             <Group justify="space-between" mt="" mb="xs">
+        //                 <Text fw={500}>{shelf.name}</Text>
+        //             </Group>
+
+        //             <Text size="sm" c="dimmed">
+        //                 Last update: {shelf.updated_at}
+        //             </Text>
+
+        //             <Button color="blue" fullWidth mt="md" radius="md" component={Link} href={`/collections/${shelf.id}`}>
+        //                 View all books
+        //             </Button>
+        //         </Card>
+        //     ))}
+        // </div>
+
         <div>
             {bookshelves.map((shelf) => (
-                <Card shadow="sm" padding="lg" radius="md" withBorder style={{ width: '60%' }}>
-                    <Group justify="space-between" mt="" mb="xs">
-                        <Text fw={500}>{shelf.name}</Text>
-                    </Group>
-
-                    <Text size="sm" c="dimmed">
-                        Last update: {shelf.updated_at}
-                    </Text>
-
-                    <Button color="blue" fullWidth mt="md" radius="md" component={Link} href={`/collections/${shelf.id}`}>
-                        View all books
-                    </Button>
-                </Card>
-            ))}
+                <CarouselCards bookshelf={shelf}></CarouselCards>
+            ))};
         </div>
     );
 }
