7710bf555ce7cbcc626a330e4e344c8015913bad
Den <denis100703@gmail.com>
2024-07-18 20:00:46 +0300

    feat: create Book page

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,11 +1,106 @@
 'use client';
 
-// import { useSearchParams } from 'next/navigation';
-import Book from '@/src/components/Book/Book';
+import { useParams } from 'next/navigation';
+import { Card, Container, Grid, Image, Text } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import { useAuthContext } from '@/src/firebase/context';
+import { Book } from '@/src/types/api';
+
+const API_HOST = process.env.NEXT_PUBLIC_API_HOST || 'http://localhost:8080/api';
 
 export default function SearchBar() {
-    // const searchParams = useSearchParams();
-    // const bookId = searchParams.get('q');
+    const { id } = useParams<{ id: string }>();
 
-    return <Book></Book>;
+    const { user, loading: userloading } = useAuthContext();
+    const [book, setBooks] = useState<Book | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const fetchBookshelves = async () => {
+            if (userloading) {
+                return;
+            }
+
+            if (!user) {
+                setLoading(false);
+                return;
+            }
+            try {
+                const token = await user?.getIdToken();
+                const res = await fetch(`${API_HOST}/books/${id}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                const data: Book = await res.json();
+                setBooks(data);
+            } catch (error) {
+                console.error('Error fetching bookshelves:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchBookshelves();
+    }, [user]);
+    // console.log(bookshelves);
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (book === null) {
+        return <div>Book not foung</div>;
+    }
+
+    return (
+        <Container size="md">
+            <Grid>
+                <Grid.Col span={6}>
+                    <Card shadow="sm" padding={0} radius="md">
+                        <Image src={book?.cover_image} alt="Book Cover" />
+                    </Card>
+                </Grid.Col>
+                <Grid.Col span={6}>
+                    <Card shadow="sm" padding="lg" radius="md" withBorder>
+                        <Grid columns={2}>
+                            <Grid.Col span={1}>
+                                <Text fw={700} size="lg">
+                                    Название
+                                </Text>
+                            </Grid.Col>
+                            <Grid.Col span={1}>
+                                <Text>{book?.title}</Text>
+                            </Grid.Col>
+                            <Grid.Col span={1}>
+                                <Text fw={700} size="lg">
+                                    Автор
+                                </Text>
+                            </Grid.Col>
+                            <Grid.Col span={1}>
+                                <Text>{book?.author}</Text>
+                            </Grid.Col>
+                            <Grid.Col span={1}>
+                                <Text fw={700} size="lg">
+                                    Описание
+                                </Text>
+                            </Grid.Col>
+                            <Grid.Col span={1}>
+                                <Text>{book?.description}</Text>
+                            </Grid.Col>
+                            <Grid.Col span={1}>
+                                <Text fw={700} size="lg">
+                                    Добавлено
+                                </Text>
+                            </Grid.Col>
+                            <Grid.Col span={1}>
+                                <Text>{book?.created_at}</Text>
+                            </Grid.Col>
+                        </Grid>
+                    </Card>
+                </Grid.Col>
+            </Grid>
+        </Container>
+    );
 }
